2877d66af03422c1a62df73dfa8d2edc7c4d637a
Lam Nhan <lamnhan92@gmail.com>
2018-11-10 06:00:23 +0700

    add tests

--- tests/test.ts
+++ tests/test.ts
@@ -22,6 +22,9 @@ describe('Test module members', () => {
     it('.Router should exist', () => {
         expect(Sheetbase.Router).to.not.null;
     });
+    it('.Utils should exist', () => {
+        expect(Sheetbase.Utils).to.not.null;
+    });
 });
 
 describe('HttpService test', () => {
@@ -83,5 +86,49 @@ describe('RouterService test', () => {
     it('#put should work');
     it('#patch should work');
     it('#delete should work');
+
+});
+
+describe('UtilsService test', () => {
+
+    const Utils = Sheetbase.Utils;
+
+    it('#o2a should work', () => {
+        const OBJ = { a: 1, b: 2, c: { c1: 1, c2: 2 } };
+        const ARR = [
+            { $key: 'a', value: 1 },
+            { $key: 'b', value: 2 },
+            { $key: 'c', c1: 1, c2: 2 },
+        ];
+        const arr = Utils.o2a(OBJ);
+        expect(arr).deep.equal(ARR);
+    });
+
+    it('#a2o should work', () => {
+        const ARR = [{id: 'a', a: 1}, {key: 'b', b: 1}, {slug: 'c', b: 1}];
+        const OBJ = {
+            a: {id: 'a', a: 1},
+            b: {key: 'b', b: 1},
+            c: {slug: 'c', b: 1},
+        };
+        const obj = Utils.a2o(ARR);
+        expect(obj).deep.equal(OBJ);
+    });
+
+    it('#uniqueId should works', () => {
+        const id = Utils.uniqueId();
+        expect(id.substr(0, 1)).to.equal('-');
+        expect(id.length).to.equal(12);
+    });
+
+    it('#uniqueId should create id (32 chars)', () => {
+        const id = Utils.uniqueId(32);
+        expect(id.length).to.equal(32);
+    });
+
+    it('#uniqueId should create id (12 chars, start with 1)', () => {
+        const id = Utils.uniqueId(null, '1');
+        expect(id.substr(0, 1)).to.equal('1');
+    });
 
 });
