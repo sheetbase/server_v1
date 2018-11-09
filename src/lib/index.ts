7f564a7dc0ce51dd4041c75d8cd9a6ed09be436f
Lam Nhan <lamnhan92@gmail.com>
2018-11-10 05:52:24 +0700

    export all classes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,7 @@ import { HttpService } from './http';
 import { RequestService } from './request';
 import { ResponseService } from './response';
 import { RouterService } from './router';
+import { UtilsService } from './utils';
 
 export function sheetbase(options?: Options) {
     const Option = new OptionService(options);
@@ -12,11 +13,13 @@ export function sheetbase(options?: Options) {
     const Request = new RequestService();
     const Response = new ResponseService(Option);
     const HTTP = new HttpService(Option, Response, Router);
+    const Utils = new UtilsService();
     return {
         Option,
         Router,
         Request,
         Response,
         HTTP,
+        Utils,
     };
 }
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,3 +1,9 @@
+export * from './lib/http';
+export * from './lib/option';
+export * from './lib/request';
+export * from './lib/response';
+export * from './lib/router';
+export * from './lib/utils';
 export * from './lib/index';
 
 export * from './lib/types';
