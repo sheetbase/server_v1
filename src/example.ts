d70cd8541e078a82e28df92d353b29238e8e2bb3
Lam Nhan <lamnhan92@gmail.com>
2018-11-11 15:36:37 +0700

    fix examples

--- src/example.ts
+++ src/example.ts
@@ -1,19 +1,17 @@
 import * as Sheetbase from './public_api';
 
-function app() {
+function load() {
     return Sheetbase.sheetbase({ views: 'views' });
 }
 
 export function example1() {
-    const { Option } = app();
+    const Sheetbase = load();
 
-    const views = Option.get('views');
+    const views = Sheetbase.Option.get('views');
     Logger.log(views);
 }
 
 export function example2(): void {
-    const { Utils } = app();
-
     const o = {
         a: 1,
         b: 2,
@@ -22,29 +20,23 @@ export function example2(): void {
             c2: 2,
         },
     };
-    const a = Utils.o2a(o);
+    const a = Sheetbase.o2a(o);
     Logger.log(a);
 }
 
 export function example3(): void {
-    const { Utils } = app();
-
     const a = [1, 2, {a: 1, b: 2, c: 3}, {key: 'd', d1: 2, d2: 2}];
-    const o = Utils.a2o(a);
+    const o = Sheetbase.a2o(a);
     Logger.log(o);
 }
 
 export function example4(): void {
-    const { Utils } = app();
-
-    Logger.log( Utils.uniqueId() );
-    Logger.log( Utils.uniqueId(32) );
-    Logger.log( Utils.uniqueId(12, '1') );
+    Logger.log( Sheetbase.uniqueId() );
+    Logger.log( Sheetbase.uniqueId(32) );
+    Logger.log( Sheetbase.uniqueId(12, '1') );
 }
 
 export function example5(): void {
-    const { Utils } = app();
-
     const o = {
         a: 1,
         a1: '1',
@@ -54,6 +46,6 @@ export function example5(): void {
         c1: '{ "c1": 1, "c2": 2 }',
         d: null,
     };
-    const output = Utils.honorData(o);
+    const output = Sheetbase.honorData(o);
     Logger.log(output);
 }
