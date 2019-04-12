1f09877b011b4f8915b0a260be036fff024f83d7
Lam Nhan <lamnhan92@gmail.com>
2019-04-12 21:15:40 +0700

    remove dist folder

--- dist/lib/request.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpEvent } from './types';
-export declare class RequestService {
-    constructor();
-    query(e?: HttpEvent): any;
-    params(e?: HttpEvent): any;
-    body(e?: HttpEvent): {};
-}
--- dist/public_api.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/index';
-export * from './lib/types';
-export * from './lib/http';
-export * from './lib/option';
-export * from './lib/request';
-export * from './lib/response';
-export * from './lib/router';
